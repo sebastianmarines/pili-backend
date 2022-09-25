618a5cf2a1fd5621584bcded22b0446674903472
Sebastian Marines <sebastian0marines@gmail.com>
2022-09-25 03:14:00 -0500

    cors

--- main.py
+++ main.py
@@ -2,8 +2,17 @@ from datetime import date
 from fastapi import FastAPI
 from config import initiate_db, Settings
 from models import Drug, DrugData
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.on_event("startup")
